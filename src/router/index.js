03c6e0f2e29445c9d9abf23b04ddfa7c0f238b37
Filip <ozdoba089@gmail.com>
2023-05-02 16:31:34 +0200

    feat(Admin): Main admin dashboard panel (#1)

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { createRouter, createWebHistory } from 'vue-router'
+import AdminDashboard from '../views/admin/AdminDashboard.vue'
+import Users from '../views/admin/Users.vue'
 const routes = [
+
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/admin',
+    name: 'admin',
+    component: AdminDashboard
   },
+
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/admin/users',
+    name: 'users',
+    component: Users
   }
+
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
---
+++ src/views/admin/AdminDashboard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="dashboard flex flex-row">
+        <div class="basis-1/6"><Menu /></div>
+        <div>asdsa</div>
+    </div>
+</template>
+<script>
+
+import Menu from '../../components/admin/Menu.vue'
+export default {
+    components: {
+    Menu
+  },
+}
+</script>
